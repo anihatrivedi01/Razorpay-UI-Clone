715c9418ec3c846f682f515af33381da98f0cd18
anihatrivedi01 <anihatrivedi05@gmail.com>
2025-12-29 09:36:21 +0530

    Add Razorpay UI clone using HTML and Tailwind CSS

---
+++ playground.js
@@ -0,0 +1,8 @@
+const markup = `<nav class="bg-orange-500" >
+    <h1 class="text-xl font-bold text-center">Hello Aniha 🚀</h1>
+   </nav> 
+   `;
+import feather from 'feather-icons';
+
+feather.replace(); // This will scan the DOM and replace all feather icons
+
